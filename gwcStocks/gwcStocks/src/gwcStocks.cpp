d49b36835d154a092fc64fffcfab9ff18061b900
Guy Collins <gwc593@gmail.com>
2020-07-25 10:56:45 +0100

    Very basic API

--- gwcStocks/gwcStocks/src/gwcStocks.cpp
+++ gwcStocks/gwcStocks/src/gwcStocks.cpp
@@ -1,14 +1,26 @@
 #include "gepch.h"
 #include "Core/URLData.h"
-const char* Finnhub_APIKey = "bs2omovrh5r9f6apl86g";
 
 
+std::string getStockDataJson(const char* dataRef, const char* symbol)
+{
+	gwcStock::URLData* url = gwcStock::URLData::GetInstance();
+
+	std::string dataToken = dataRef;
+	std::string header = "https://uk.finance.yahoo.com/quote/";
+	std::string footer = "?p=RR.L&.tsrc=fin-srch";
+	std::string _symbol = symbol;
+
+	std::string result = url->CurlURL(header + _symbol + footer);
+
+	auto price = result.substr(result.find(dataToken));
+	price.erase(price.find('}') + 1, price.size());
+
+	return price;
+}
 
 int main()
 {	
-	gwcStock::URLData* url = gwcStock::URLData::GetInstance();
-	std::string result = url->CurlURL("https://finnhub.io/api/v1/stock/symbol?exchange=L&token=bs2omovrh5r9f6apl86g");
-
-	std::cout << result << std::endl;
+	std::cout << getStockDataJson("currentPrice","RR.L") << std::endl;
 	return 0;
 }
