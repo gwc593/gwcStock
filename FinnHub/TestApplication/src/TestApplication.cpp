e73f75a308edec9b6d14f3a4b67ba19746492e38
Guy Collins <gwc593@gmail.com>
2020-07-27 11:10:46 +0100

    added testing schedule mechanism.

--- FinnHub/TestApplication/src/TestApplication.cpp
+++ FinnHub/TestApplication/src/TestApplication.cpp
@@ -3,7 +3,17 @@
 
 /// !!!! READ ME FINNHUB REP!!!!!///
 // please run in Release mode to test speed, the for loop for populating std::vector<FinnHub::Symbol> symbols; ~ line 17 is inherantly very slow in debug mode because of the number of debug symbols. 
-// if you want to run in debug, please comment the afformentioned section out.
+// if you want to run in debug, please set testGetSymbols to false.
+
+
+//Testing profile
+bool testGetSymbols			= false; //Slow in debug mode
+bool testGetQuote			= true;
+bool testGetCompanyProfile2 = true;
+bool testGetNews			= true;
+bool testGetCompanyNews		= true;
+bool testGetNewsSentiment	= true;
+
 
 int main()
 {
@@ -11,56 +21,85 @@ int main()
 	/////////////////////////////////////
 	FinnHubAPI dataExchange("bse5d8frh5rea8raakb0");
 
-
 	//get all available symbols from US stock exchange
 	//////////////////////////////////////////////////
-	std::string symbolDataJSON = dataExchange.GetSymbols("US");
-	auto numberOfSymbols = JSON::NumObjects(symbolDataJSON);
+	std::string symbolsJSON;
+	int32_t numberOfSymbols;
 	std::vector<FinnHub::Symbol> symbols;
-	for (int i = 0; i < numberOfSymbols; i++) {
-		FinnHub::Symbol tmp;
-		tmp.Deserialise(JSON::GetObjDataN(symbolDataJSON, i));
-		symbols.push_back(tmp);
+
+	if (::testGetSymbols) {
+		symbolsJSON = dataExchange.GetSymbols("US");
+		numberOfSymbols = JSON::NumObjects(symbolsJSON);
+		for (int i = 0; i < numberOfSymbols; i++) {
+			FinnHub::Symbol tmp;
+			tmp.Deserialise(JSON::GetObjDataN(symbolsJSON, i));
+			symbols.push_back(tmp);
+		}
 	}
+
 
 	//Get quote for AAPL and store it in a FinnHub Candle Object
 	/////////////////////////////////////////////////////////////
-	std::string candleJSON = dataExchange.GetQuote("AAPL");
+	std::string candleJSON;
 	FinnHub::Candle myCandle;
-	myCandle.Deserialise(candleJSON);
+
+	if (::testGetQuote) {
+		candleJSON = dataExchange.GetQuote("AAPL");
+		myCandle.Deserialise(candleJSON);
+	}
+
 
 	//Get Company Profile2 for AAPL
 	///////////////////////////////
-	std::string companyProfile2JSON = dataExchange.GetCompanyProfile2("AAPL");
+	std::string companyProfile2JSON;
 	FinnHub::CompanyProfile companyProfleObj;
-	companyProfleObj.Deserialise(companyProfile2JSON);
 
+	if (::testGetCompanyProfile2) {
+		companyProfile2JSON = dataExchange.GetCompanyProfile2("AAPL");
+		companyProfleObj.Deserialise(companyProfile2JSON);
+	}
 
 	//GetNews
 	//////////
-	std::string newsJSON = dataExchange.GetNews("general", 0);
+	std::string newsJSON;
 	std::vector<FinnHub::News> articles;
-	auto numberOfArticles = JSON::NumObjects(newsJSON);
-	for (int i = 0; i < numberOfArticles; i++) {
-		FinnHub::News tmp;
-		tmp.Deserialise(JSON::GetObjDataN(newsJSON, i));
-		articles.push_back(tmp);
+	int32_t numberOfArticles;
+
+	if (::testGetNews) {
+		newsJSON = dataExchange.GetNews("general", 0);
+		numberOfArticles = JSON::NumObjects(newsJSON);
+		for (int i = 0; i < numberOfArticles; i++) {
+			FinnHub::News tmp;
+			tmp.Deserialise(JSON::GetObjDataN(newsJSON, i));
+			articles.push_back(tmp);
+		}
 	}
 
+
 	//GetCompanyNews
-	std::string companyNewsJSON = dataExchange.GetCompanyNews("AAPL");
+	std::string companyNewsJSON;
 	std::vector<FinnHub::News> companyArticles;
-	auto numberOfCompanyArticles = JSON::NumObjects(companyNewsJSON);
-	for (int i = 0; i < numberOfCompanyArticles; i++) {
-		FinnHub::News tmp;
-		tmp.Deserialise(JSON::GetObjDataN(companyNewsJSON, i));
-		companyArticles.push_back(tmp);
+	int32_t numberOfCompanyArticles;
+
+	if (::testGetCompanyNews) {
+		companyNewsJSON = dataExchange.GetCompanyNews("AAPL");
+		numberOfCompanyArticles = JSON::NumObjects(companyNewsJSON);
+		for (int i = 0; i < numberOfCompanyArticles; i++) {
+			FinnHub::News tmp;
+			tmp.Deserialise(JSON::GetObjDataN(companyNewsJSON, i));
+			companyArticles.push_back(tmp);
+		}
 	}
 
 	
-
 	//Get News Sentiment
-	std::string newsSentimentJSON = dataExchange.GetNewsSentiment("AAPL");
+	std::string newsSentimentJSON;
 	FinnHub::NewsSentiment newsSentiment;
-	newsSentiment.Deserialise(newsSentimentJSON);
+
+	if (::testGetNewsSentiment) {
+		newsSentimentJSON = dataExchange.GetNewsSentiment("AAPL");
+		newsSentiment.Deserialise(newsSentimentJSON);
+	}
+
+	std::cout << "finished" << std::endl;
 }
